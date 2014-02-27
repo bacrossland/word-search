ff0eac3a948d868b705b431486bb020209539201
bacrossland <bacrossland@gmail.com>
2014-02-27 01:26:17 -0600

    Added the tokenizer and tests.

--- lib/word_search.rb
+++ lib/word_search.rb
@@ -2,17 +2,48 @@ require 'pathname'
 
 class WordSearch
   
-  # Initialize a WordSearch object. Requires a valid file path be passed in.
-  def initialize(fp = nil)
+  WS_CONFIG = YAML.load_file("config/word_search.yaml")
+  DICTIONARY_PATH = WS_CONFIG["dictionary_path"]
+  TOKEN_SIZE = WS_CONFIG["token_size"]
+  
+  # Initialize a WordSearch object. Requires a valid file path be passed in. Defaults to dictionary_path
+  # in config/word_search.yaml.
+  def initialize(fp = DICTIONARY_PATH)
     if fp.nil?
-      raise ArgumentError.new("WordSearch requires a path to a dictionary file to load.")
+      raise ArgumentError.new("WordSearch requires a path to a dictionary file to load. Please add the path to that file in config/word_search.yaml.")
     else
       @dictionary_file = Pathname.new(fp)
       if !@dictionary_file.exist?
         @dictionary_file = nil
-        raise ArgumentError.new("The file '#{fp}' does not exist.")
+        raise ArgumentError.new("The file '#{fp}' does not exist. Please check the file path for the dictionary file. Hint: This is normally set in config/word_search.yaml.")
       end    
     end    
   end
+  
+  private
+  
+  # This method splits a String into tokens of the size specified. Defaults to a token size of 1. 
+  # Takes a String and optional Integer or Fixnum. Returns an Array.
+  def tokenizer(str, token_size = 1)
+    # Treat token_size of 0 as if it was a 1.
+    if token_size == 0
+      token_size = 1
+    end
     
+    if token_size == 1
+      token_array = str.split("")
+    else
+      token_array = []
+      ul = str.length - 1
+      ll = 0
+      ts = token_size - 1
+      while ts <= ul
+        token_array << str[ll..ts]
+        ll += 1
+        ts += 1
+      end 
+    end
+    
+    return token_array
+  end  
 end
--- test/word_search_test.rb
+++ test/word_search_test.rb
@@ -3,22 +3,46 @@ require 'pathname'
 require 'yaml'
 require 'word_search'
 
+# Make private WordSearch methods public for testing.
+class WordSearch
+    public *self.private_instance_methods(false)
+end
+
 class WordSearchTest < Test::Unit::TestCase
   WS_CONFIG = YAML.load_file("config/word_search.yaml")
   DICTIONARY_PATH = WS_CONFIG["dictionary_path"]
+  TOKEN_SIZE = WS_CONFIG["token_size"]
   
   # This test checks to see if dictionary file is present. 
   def test_config
     dictionary_path = Pathname.new(DICTIONARY_PATH)
     assert(dictionary_path.exist?, "The dictionary_path file '#{DICTIONARY_PATH}' does not exist.")
+    assert_instance_of(Fixnum, TOKEN_SIZE) unless TOKEN_SIZE.nil?
   end
   
   # A test of the WordSearch class initialize method.
   def test_word_search_init
-    assert_raise(ArgumentError){WordSearch.new}
     assert_raise(ArgumentError){WordSearch.new('sdsdf.txt')}
-    assert_nothing_raised{WordSearch.new(DICTIONARY_PATH)}    
+    assert_nothing_raised{WordSearch.new}    
   end
+  
+  # A test of the tokenizer WordSearch private instance method.
+  def test_tokenizer
+    a = WordSearch.new.tokenizer("guild")
+    b = WordSearch.new.tokenizer("guild", 2)
+    c = WordSearch.new.tokenizer("guild", 3)
+    d = WordSearch.new.tokenizer("guild", 4)
+    e = WordSearch.new.tokenizer("guild", 5)
+    f = WordSearch.new.tokenizer("guild", 0)
+    assert_instance_of(Array, a)
+    assert_equal(["g","u","i","l","d"], a)
+    assert_equal(["gu","ui","il","ld"], b)
+    assert_equal(["gui","uil","ild"], c)
+    assert_equal(["guil","uild"], d)
+    assert_equal(["guild"], e)
+    assert_equal(["g","u","i","l","d"], f)
+  end  
+    
 end
 
 
